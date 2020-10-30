2b9d81b96f6d73b401e14a900256287757cb1030
thomas@bookmemate.com <thomas@bookmemate.com>
2020-10-30 17:39:48 +1100

    Created a divider component

---
+++ src/components/Divider.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import styled from 'styled-components/native';
+
+import { ContainerProps, Colors } from '../styles';
+
+type ContainerType = {
+  mTop: number;
+  mBottom: number;
+  color: string;
+};
+
+const Container = styled.View<ContainerType>`
+  ${ContainerProps.column}
+  ${ContainerProps.grow}
+	background-color: ${Colors.transparent};
+  height: 2px;
+  border-top-width: 1px;
+  border-color: ${(props) => props.color};
+  margin-top: ${(props) => `${props.mTop}px`};
+  margin-bottom: ${(props) => `${props.mBottom}px`};
+`;
+
+interface IProps {
+  mTop?: number;
+  mBottom?: number;
+  color?: string;
+}
+
+export const Divider: FC<IProps> = ({
+  mTop = 15,
+  mBottom = 15,
+  color = Colors.cursedGrey,
+}) => <Container mTop={mTop} mBottom={mBottom} color={color} />;
