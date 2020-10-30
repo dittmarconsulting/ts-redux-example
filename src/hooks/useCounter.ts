fe8762e7a7ae264432bfc03c7745aa815554caa9
thomas@bookmemate.com <thomas@bookmemate.com>
2020-10-30 15:48:34 +1100

    Set the correct return type for the hook

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { ActionCreator } from '../redux/actions';
 import { IProductState } from '../redux/types/reducers';
 
-export const useCounter = (initValue: number) => {
+export const useCounter = (
+  initValue: number,
+): [number, Dispatch<SetStateAction<number>>] => {
   const quantityState = useSelector(
     (state: { productState: IProductState }) => state?.productState?.quantity,
   );
   const dispatch = useDispatch();
 
-  const [quantity, setQuantity] = useState<number>(
-    quantityState ? quantityState : initValue,
-  );
+  const [quantity, setQuantity] = useState<number>(quantityState || initValue);
 
   useEffect(() => {
     dispatch(ActionCreator.setQuantity(quantity));
